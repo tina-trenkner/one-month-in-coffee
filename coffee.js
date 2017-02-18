830ae2b2feec49b0033a4b4f9cbe337cbc34007c
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2017-02-18 20:11:26 +1300

    first commit

---
+++ coffee.js
@@ -0,0 +1,6 @@
+function yearInCoffee() {
+  const jan = document.getElementById("january").innerHTML;
+  const janCoffee = jan.replace("_____ _____ _____ _____", "_____ ____! __!!_ !@@@_");
+  document.getElementById("january").innerHTML = janCoffee;
+}
+ 
