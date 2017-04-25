ca6f57cd98fc13800277251a4fa318d0a3f8bdd1
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2017-04-25 12:31:13 +1200

    new feature: monthly diary note

--- coffee.js
+++ coffee.js
@@ -12,14 +12,23 @@ function febInCoffee() {
 
 function marInCoffee() {
  const mar = document.getElementById("march").innerHTML;
- const marCoffee = mar.replace("_____ _____ _____ _____ _", "_!!!! _!!__ __!__ _@__! _ ");
- document.getElementById("march").innerHTML = marchCoffee;
+ const marCoffee = mar.replace("_____ _____ _____ _____ _____", "_!!!! _!!__ __!__ _@__! @!!!!");
+ document.getElementById("march").innerHTML = marCoffee;
+}
+
+function aprInCoffee() {
+ const apr = document.getElementById("april").innerHTML;
+ const aprCoffee = apr.replace("_____ _____ _____ __", "!_!__ @____ __!!! __");
+ document.getElementById("april").innerHTML = aprCoffee;
 }
 
 setTimeout(function () {janInCoffee()}, 3000)
 setTimeout(function () {febInCoffee()}, 5000)
-setTimeout(function () {marchInCoffee()}, 7000)
+setTimeout(function () {marInCoffee()}, 7000)
+setTimeout(function () {aprInCoffee()}, 9000)
 
 //onLoad then execute function that takes a letter, hides it, then changes it
 //or use string.replace() somehow
+
+//Could likely refactor by putting the strings into an array and running a loop. Haven't quite thought this through, but the functions are clearly formulaic.
 //or put each day in a span and change what's in the span tags
