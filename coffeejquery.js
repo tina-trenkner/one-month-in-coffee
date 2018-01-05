1135d1445663abca4e095d1b247d9bb4a5e796fb
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2018-01-05 16:40:03 +1300

    jquery code for animating the character swapouts

--- coffeejquery.js
+++ coffeejquery.js
@@ -1,30 +1,29 @@
 $(document).ready(function(){
   oct30();
-  setTimeout(oct31, 1000);
-  // setTimeout(nov1, 2000);
-  // setTimeout(nov2, 3500);
-  // setTimeout(nov3, 4000);
-  // setTimeout(nov4, 5000);
-  // setTimeout(nov7, 6000);
-  // setTimeout(nov8, 7000);
-  // setTimeout(nov9, 8000);
-  // setTimeout(nov10, 9000);
-  // setTimeout(nov11, 10000);
-  // setTimeout(nov14, 11000);
-  // setTimeout(nov15, 12000);
-  // setTimeout(nov16, 13000);
-  // setTimeout(nov17, 14000);
-  // setTimeout(nov18, 15000);
-  // setTimeout(nov21, 16000);
-  // setTimeout(nov22, 17000);
-  // setTimeout(nov23, 18000);
-  // setTimeout(nov24, 19000);
-  // setTimeout(nov25, 20000);
-  // setTimeout(nov28, 21000);
-  // setTimeout(nov29, 22000);
-  // setTimeout(nov30, 23000);
-  // setTimeout(nov31, 24000);
-  // setTimeout(dec1, 25000);
+  setTimeout(oct31, 2500);
+  setTimeout(nov1, 3000);
+  setTimeout(nov2, 4000);
+  setTimeout(nov3, 5000);
+  setTimeout(nov6, 6000);
+  setTimeout(nov7, 7000);
+  setTimeout(nov8, 8000);
+  setTimeout(nov9, 9000);
+  setTimeout(nov10, 10000);
+  setTimeout(nov13, 11000);
+  setTimeout(nov14, 12000);
+  setTimeout(nov15, 13000);
+  setTimeout(nov16, 14000);
+  setTimeout(nov17, 15000);
+  setTimeout(nov20, 16000);
+  setTimeout(nov21, 17000);
+  setTimeout(nov22, 18000);
+  setTimeout(nov23, 19000);
+  setTimeout(nov24, 20000);
+  setTimeout(nov27, 21000);
+  setTimeout(nov28, 22000);
+  setTimeout(nov29, 23000);
+  setTimeout(nov30, 24000);
+  setTimeout(dec1, 25000);
 })
 
 function oct30() {
@@ -47,13 +46,283 @@ function oct31() {
   $("#oct31").show("fast", showOct31Coffee);
 }
 
-// // function nov1() {
-// //   $("#nov1").fadeOut(300);
-// //   $("#nov1").fadeIn(300);
-// //   $("#nov1").fadeOut(300);
-// //   $("#nov1").fadeIn(300);
-// // }
-//
+function nov1() {
+  $("#nov1").fadeOut(300);
+  $("#nov1").fadeIn(300);
+  $("#nov1").fadeOut(300);
+  $("#nov1").fadeIn(300);
+}
+
+function nov2() {
+  $("#nov2").fadeOut(300);
+  $("#nov2").fadeIn(300);
+  $("#nov2").fadeOut(300);
+  $("#nov2").fadeIn(300);
+}
+
+function nov3() {
+  $("#nov3").fadeOut(300);
+  $("#nov3").fadeIn(300);
+  $("#nov3").fadeOut(300);
+  $("#nov3").fadeIn(300);
+  $("#nov3").fadeOut(300);
+  $("#nov3").hide(500);
+  $("#nov3").show("fast", showNov3Coffee);
+}
+
+function nov6() {
+  $("#nov6").fadeOut(300);
+  $("#nov6").fadeIn(300);
+  $("#nov6").fadeOut(300);
+  $("#nov6").fadeIn(300);
+}
+
+function nov7() {
+  $("#nov7").fadeOut(300);
+  $("#nov7").fadeIn(300);
+  $("#nov7").fadeOut(300);
+  $("#nov7").fadeIn(300);
+}
+
+function nov8() {
+  $("#nov8").fadeOut(300);
+  $("#nov8").fadeIn(300);
+  $("#nov8").fadeOut(300);
+  $("#nov8").fadeIn(300);
+  $("#nov8").fadeOut(300);
+  $("#nov8").hide(500);
+  $("#nov8").show("fast", showNov8Coffee);
+}
+
+function nov9() {
+  $("#nov9").fadeOut(300);
+  $("#nov9").fadeIn(300);
+  $("#nov9").fadeOut(300);
+  $("#nov9").fadeIn(300);
+  $("#nov9").fadeOut(300);
+  $("#nov9").hide(500);
+  $("#nov9").show("fast", showNov9Coffee);
+}
+
+function nov10() {
+  $("#nov10").fadeOut(300);
+  $("#nov10").fadeIn(300);
+  $("#nov10").fadeOut(300);
+  $("#nov10").fadeIn(300);
+}
+
+function nov13() {
+  $("#nov13").fadeOut(300);
+  $("#nov13").fadeIn(300);
+  $("#nov13").fadeOut(300);
+  $("#nov13").fadeIn(300);
+  $("#nov13").fadeOut(300);
+  $("#nov13").hide(500);
+  $("#nov13").show("fast", showNov13Coffee);
+}
+
+function nov14() {
+  $("#nov14").fadeOut(300);
+  $("#nov14").fadeIn(300);
+  $("#nov14").fadeOut(300);
+  $("#nov14").fadeIn(300);
+}
+
+function nov15() {
+  $("#nov15").fadeOut(300);
+  $("#nov15").fadeIn(300);
+  $("#nov15").fadeOut(300);
+  $("#nov15").fadeIn(300);
+  $("#nov15").fadeOut(300);
+  $("#nov15").hide(500);
+  $("#nov15").show("fast", showNov15Coffee);
+}
+
+function nov16() {
+  $("#nov16").fadeOut(300);
+  $("#nov16").fadeIn(300);
+  $("#nov16").fadeOut(300);
+  $("#nov16").fadeIn(300);
+}
+
+function nov17() {
+  $("#nov17").fadeOut(300);
+  $("#nov17").fadeIn(300);
+  $("#nov17").fadeOut(300);
+  $("#nov17").fadeIn(300);
+}
+
+function nov20() {
+  $("#nov20").fadeOut(300);
+  $("#nov20").fadeIn(300);
+  $("#nov20").fadeOut(300);
+  $("#nov20").fadeIn(300);
+}
+
+function nov21() {
+  $("#nov21").fadeOut(300);
+  $("#nov21").fadeIn(300);
+  $("#nov21").fadeOut(300);
+  $("#nov21").fadeIn(300);
+  $("#nov21").fadeOut(300);
+  $("#nov21").hide(500);
+  $("#nov21").show("fast", showNov21Coffee);
+}
+
+function nov22() {
+  $("#nov22").fadeOut(300);
+  $("#nov22").fadeIn(300);
+  $("#nov22").fadeOut(300);
+  $("#nov22").fadeIn(300);
+}
+
+function nov23() {
+  $("#nov23").fadeOut(300);
+  $("#nov23").fadeIn(300);
+  $("#nov23").fadeOut(300);
+  $("#nov23").fadeIn(300);
+  $("#nov23").fadeOut(300);
+  $("#nov23").hide(500);
+  $("#nov23").show("fast", showNov23Coffee);
+}
+
+function nov24() {
+  $("#nov24").fadeOut(300);
+  $("#nov24").fadeIn(300);
+  $("#nov24").fadeOut(300);
+  $("#nov24").fadeIn(300);
+}
+
+function nov27() {
+  $("#nov27").fadeOut(300);
+  $("#nov27").fadeIn(300);
+  $("#nov27").fadeOut(300);
+  $("#nov27").fadeIn(300);
+}
+
+function nov28() {
+  $("#nov28").fadeOut(300);
+  $("#nov28").fadeIn(300);
+  $("#nov28").fadeOut(300);
+  $("#nov28").fadeIn(300);
+  $("#nov28").fadeOut(300);
+  $("#nov28").hide(500);
+  $("#nov28").show("fast", showNov28Coffee);
+}
+
+function nov29() {
+  $("#nov29").fadeOut(300);
+  $("#nov29").fadeIn(300);
+  $("#nov29").fadeOut(300);
+  $("#nov29").fadeIn(300);
+  $("#nov29").fadeOut(300);
+  $("#nov29").hide(500);
+  $("#nov29").show("fast", showNov29Coffee);
+}
+
+function nov30() {
+  $("#nov30").fadeOut(300);
+  $("#nov30").fadeIn(300);
+  $("#nov30").fadeOut(300);
+  $("#nov30").fadeIn(300);
+  $("#nov30").fadeOut(300);
+  $("#nov30").hide(500);
+  $("#nov30").show("fast", showNov30Coffee);
+}
+
+function dec1() {
+  $("#dec1").fadeOut(300);
+  $("#dec1").fadeIn(300);
+  $("#dec1").fadeOut(300);
+  $("#dec1").fadeIn(300);
+}
+
+
+function showOct30Coffee() {
+  const two = document.getElementById("oct30").innerHTML;
+  const twoCoffee = two.replace("_", "@");
+  document.getElementById("oct30").innerHTML  = twoCoffee;
+  $("#oct30").show()
+}
+
+function showOct31Coffee() {
+  const two = document.getElementById("oct31").innerHTML;
+  const twoCoffee = two.replace("_", "@");
+  document.getElementById("oct31").innerHTML  = twoCoffee;
+  $("#oct31").show()
+}
+
+function showNov3Coffee() {
+  const two = document.getElementById("nov3").innerHTML;
+  const twoCoffee = two.replace("_", "@");
+  document.getElementById("nov3").innerHTML  = twoCoffee;
+  $("#nov3").show()
+}
+
+function showNov8Coffee() {
+  var one = document.getElementById("nov8").innerHTML;
+  const oneCoffee = one.replace("_", "!");
+  document.getElementById("nov8").innerHTML  = oneCoffee;
+  $("#nov8").show();
+}
+
+function showNov9Coffee() {
+  const two = document.getElementById("nov9").innerHTML;
+  const twoCoffee = two.replace("_", "@");
+  document.getElementById("nov9").innerHTML  = twoCoffee;
+  $("#nov9").show()
+}
+
+function showNov13Coffee() {
+  var one = document.getElementById("nov13").innerHTML;
+  const oneCoffee = one.replace("_", "!");
+  document.getElementById("nov13").innerHTML  = oneCoffee;
+  $("#nov13").show();
+}
+
+function showNov15Coffee() {
+  var one = document.getElementById("nov15").innerHTML;
+  const oneCoffee = one.replace("_", "!");
+  document.getElementById("nov15").innerHTML  = oneCoffee;
+  $("#nov15").show();
+}
+
+function showNov21Coffee() {
+  var one = document.getElementById("nov21").innerHTML;
+  const oneCoffee = one.replace("_", "!");
+  document.getElementById("nov21").innerHTML  = oneCoffee;
+  $("#nov21").show();
+}
+
+function showNov23Coffee() {
+  var three = document.getElementById("nov23").innerHTML;
+   const threeCoffee = three.replace("_", "#");
+   document.getElementById("nov23").innerHTML  = threeCoffee;
+  $("#nov23").show();
+}
+
+function showNov28Coffee() {
+  const two = document.getElementById("nov28").innerHTML;
+  const twoCoffee = two.replace("_", "@");
+  document.getElementById("nov28").innerHTML  = twoCoffee;
+  $("#nov28").show()
+}
+
+function showNov29Coffee() {
+  const two = document.getElementById("nov29").innerHTML;
+  const twoCoffee = two.replace("_", "@");
+  document.getElementById("nov29").innerHTML  = twoCoffee;
+  $("#nov29").show()
+}
+
+function showNov30Coffee() {
+  const two = document.getElementById("nov30").innerHTML;
+  const twoCoffee = two.replace("_", "@");
+  document.getElementById("nov30").innerHTML  = twoCoffee;
+  $("#nov30").show()
+}
+
+
 // // function nov2() {
 //   // $("#two").fadeOut(300);
 //   // $("#two").fadeIn(300);
@@ -146,19 +415,7 @@ function oct31() {
 
 
 
-function showOct30Coffee() {
-  const two = document.getElementById("oct30").innerHTML;
-  const twoCoffee = two.replace("_", "@");
-  document.getElementById("oct30").innerHTML  = twoCoffee;
-  $("#oct30").show()
-}
 
-function showOct31Coffee() {
-  const two = document.getElementById("oct31").innerHTML;
-  const twoCoffee = two.replace("_", "@");
-  document.getElementById("oct31").innerHTML  = twoCoffee;
-  $("#oct31").show()
-}
 //
 // function showNov3Coffee() {
 //    var two = document.getElementById("nov3").innerHTML;
@@ -167,15 +424,10 @@ function showOct31Coffee() {
 //   $("#nov3").show()
 // }
 //
-// function showNov8Coffee() {
-//   var one = document.getElementById("nov8").innerHTML;
-//   const oneCoffee = one.replace("_", "!");
-//   document.getElementById("nov8").innerHTML  = oneCoffee;
-//   $("#nov8").show();
-// }
+
 //
 // function showThreeCoffee() {
-//    var three = document.getElementById("three").innerHTML;
+//   var three = document.getElementById("three").innerHTML;
 //   const threeCoffee = three.replace("_", "#");
 //   document.getElementById("three").innerHTML  = threeCoffee;
 //   $("#three").show()
